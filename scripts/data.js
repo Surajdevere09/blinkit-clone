41655d360a6d66e4c85e3d949d148bfd2942b6b3
SURAJ DEVERE <suraj.devere@gmail.com>
2022-01-18 16:33:40 +0530

    changes

--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,137 @@
 // suraj start
+
+{
+    filterImage: "https://cdn.grofers.com/app/images/category/small/1459601771_Grocery-small.jpg",
+iconImage: "https://cdn.grofers.com/app/images/category/cms_images/icon/16_1478184820854.png",
+id: 16,
+image: "https://cdn.grofers.com/app/images/category/large/1459601771_grocery-staples.jpg",
+mScore: 1100,
+name: "grocery & staples",
+productCount: 1,
+subcategories:[
+    {
+        avg_disc: 16.15125,
+category_image_versions: [
+    {
+        app_version: "default",
+icon_url: "https://cdn.grofers.com/app/images/category/cms_images/icon/1010_1615986555615.png",
+id: 0,
+image_height: 256,
+image_type: "icon",
+image_url: "",
+image_width: 256 , 
+    }
+],
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+icon_image_url: "https://cdn.grofers.com/app/images/category/cms_images/icon/1010_1615986555615.png",
+id: 1010,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 51.74,
+name: "pulses",
+pc_id: 16,
+sort_order: 18,
+sub_text: "Pulses",
+subcategory:[
+    {
+        avg_disc: 18.64,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1195,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 49.55,
+name: "arhar",
+pc_id: 16,
+sort_order: 10,
+sub_text: "",
+    },
+    {
+        avg_disc: 20.11,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1196,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 44,
+name: "moong",
+pc_id: 16,
+sort_order: 9,
+sub_text: "",
+    },
+    {avg_disc: 19.09,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1197,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 51.74,
+name: "urad",
+pc_id: 16,
+sort_order: 8,
+sub_text: "",
+},
+{
+    avg_disc: 19.1,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1198,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 46.09,
+name: "rajma & chana",
+pc_id: 16,
+sort_order: 7,
+sub_text: "",
+},
+{
+    avg_disc: 13.23,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1296,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 34.44,
+name: "masoor",
+pc_id: 16,
+sort_order: 5,
+sub_text: "",
+},
+{
+    avg_disc: 10.19,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1297,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 25,
+name: "soya",
+pc_id: 16,
+sort_order: 4,
+sub_text: "",
+},
+{avg_disc: 10.03,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1009,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 33.08,
+name: "dried peas & others",
+pc_id: 16,
+sort_order: 2,
+sub_text: "Labelled Pulses"},
+{
+    avg_disc: 18.82,
+category_image_versions: null,
+filter_image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+id: 1183,
+image: "http://cdn.grofer.it/app/images/placeholders/merchant.jpg",
+max_disc: 34.12,
+name: "organic pulses",
+pc_id: 16,
+sort_order: 0,
+sub_text: "",
+},
+]
+    }
+
+]
+},
+
+
 // write your code here 
 // ------------->>>>> start here
 // suraj end
@@ -18,6 +151,7 @@
 // Nilesh end
 
 // Om  start
+
 // ------------->>>>> start here
 // Om  end
 
